0d4180a376cee0bdd2b138d2fc99cea0497dfd14
h0ru5 <johannes.hund@gmail.com>
2014-04-21 21:55:51 +0200

    added callbacks

--- src/main/java/com/github/h0ru5/gwt/websock/WebsockService.java
+++ src/main/java/com/github/h0ru5/gwt/websock/WebsockService.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.google.gwt.angular.client.NgInject;
+import com.google.gwt.core.client.Callback;
 import com.sksamuel.gwt.websockets.Websocket;
 import com.sksamuel.gwt.websockets.WebsocketListener;
 
@@ -11,29 +12,23 @@ import com.sksamuel.gwt.websockets.WebsocketListener;
 public class WebsockService {
 
 	private Map<String,Websocket> sockets = new HashMap<String, Websocket>();
-		
+
 	public String connect(String url) {
 		String newid = String.format("%d",sockets.size() + 1);
 		sockets.put(newid,new Websocket(url));
 		return newid;
 	}
-	
+
 	public Websocket connect(String url,String id) {
 		Websocket ws = new Websocket(url);
 		sockets.put(id,ws);
 		return ws;
 	}
-	
-	public void addListener(String id, WebsocketListener listener) {
-		Websocket ws = get(id);
-		if(ws!=null)
-			ws.addListener(listener);
-	}
-	
+
 	public Websocket get(String id) {
 		return sockets.get(id);
 	}
-	
+
 	public boolean isConnected(String id) {
 		Websocket ws = get(id);
 		if(ws==null)
@@ -41,12 +36,85 @@ public class WebsockService {
 		else
 			return ws != null && ws.getState() == 1;
 	}
-	
+
 	public void close(String id) {
 		Websocket ws = get(id);
 		if(ws!=null)
 			ws.close();
 	}
+
+	public void addListener(String id, WebsocketListener listener) {
+		Websocket ws = get(id);
+		if(ws!=null)
+			ws.addListener(listener);
+	}
+
+	public void onMessage(String id, final Callback<String, Void> jsfunc) {
+		Websocket ws = get(id);
+		if(ws!=null) {
+			WebsocketListener listener = new WebsocketListener() {
+
+				@Override
+				public void onOpen() {}
+
+				@Override
+				public void onMessage(String msg) {
+					jsfunc.onSuccess(msg);
+				}
+
+				@Override
+				public void onClose() {}
+			};
+			ws.addListener(listener);
+		} else {
+			jsfunc.onFailure(null);
+		}
+	}
+
+	public void onOpen(String id, final Callback<Void, Void> jsfunc) {
+		Websocket ws = get(id);
+		if(ws!=null) {
+			WebsocketListener listener = new WebsocketListener() {
+
+				@Override
+				public void onOpen() {
+					jsfunc.onSuccess(null);
+				}
+
+				@Override
+				public void onMessage(String msg) {}
+
+				@Override
+				public void onClose() {}
+			};
+			ws.addListener(listener);
+		} else {
+			jsfunc.onFailure(null);
+		}
+	}
 	
-	
+	public void onClose(String id, final Callback<Void, Void> jsfunc) {
+		Websocket ws = get(id);
+		if(ws!=null) {
+			WebsocketListener listener = new WebsocketListener() {
+
+				@Override
+				public void onOpen() {}
+
+				@Override
+				public void onMessage(String msg) {}
+
+				@Override
+				public void onClose() {
+					jsfunc.onSuccess(null);
+				}
+			};
+			ws.addListener(listener);
+		} else {
+			jsfunc.onFailure(null);
+		}
+	}
+
+	// TODO promises interface
+
 }
